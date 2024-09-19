9fc12609a39d5345c77f2991acbac2f4e877a9ca
phtai214 <toxictai2@gmail.com>
2024-09-19 17:57:33 +0700

    third commit

---
+++ Calculator/Tester2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Tester2
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 1;
+            Console.WriteLine(a - b);
+        }
+    }
+}
